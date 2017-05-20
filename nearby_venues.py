5b2a98b3983dbc13840f6ef35060b6a4845c9ad2
broschke <bernardoroschke@gmail.com>
2017-05-20 16:17:45 -0400

    updates

--- nearby_venues.py
+++ nearby_venues.py
@@ -11,28 +11,44 @@ import pprint
 from pathlib import Path
 import os
 from key import ID, SECRET
+import time
 
 CLIENT_ID = ID
 CLIENT_SECRET = SECRET
 
-url_venue = "https://api.foursquare.com/v2/venues/explore?"
+explore_url_venue = "https://api.foursquare.com/v2/venues/explore?"
 near = 'll=38.9,-77.0'
-radius = '1'
+radius = '1000'
 v = "20140806"
 m = "foursquare"
 
-url_composite = url_venue + "client_id=" + CLIENT_ID + "&" + "client_secret=" + CLIENT_SECRET + "&" + near + "&" + radius + "&" + "v=" + v + "&" + "m=" + m
-contents = urllib.request.urlopen(url_composite).read()
+explore_url_composite = explore_url_venue + "client_id=" + CLIENT_ID + "&" + "client_secret=" + CLIENT_SECRET + "&" + near + "&" + radius + "&" + "v=" + v + "&" + "m=" + m
+contents = urllib.request.urlopen(explore_url_composite).read()
 
-parsed = json.loads(contents)
+explore_parsed = json.loads(contents)
 
-#id = parsed['response']['groups'][0]['items'][0]['venue']['id']
-id = parsed['response']['groups'][0]['items']
+venue_id = explore_parsed['response']['groups'][0]['items']
 
-for i in id:
-    print(i['venue']['id'])
+venue_list = [i['venue']['id'] for i in venue_id]
 
-#pprint.pprint(parsed)
+# print(venue_list)
+ids = []
+venue = []
+rating = []
+checkins_count = []
+users_count = []
 
-#print(id)
-#print(url_composite)
+url_venue_base = "https://api.foursquare.com/v2/venues/"
+
+for i in venue_list:
+	url = url_venue_base + i + "?" + "client_id=" + CLIENT_ID + "&" + "client_secret=" + CLIENT_SECRET + "&" + "v=" + v + "&" + "m=" + m
+	contents = urllib.request.urlopen(url).read()
+	parsed = json.loads(contents)
+	ids.append(parsed['response']['venue']['id'])
+	venue.append(parsed['response']['venue']['name'])
+	rating.append(parsed['response']['venue']['rating'])
+	checkins_count.append(parsed['response']['venue']['stats']['checkinsCount'])
+	users_count.append(parsed['response']['venue']['stats']['usersCount'])
+	time.sleep(1)
+
+print(users_count)
